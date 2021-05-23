189dca95ac2ebe7135d09a3be75aaeaeeba00d33
senthil <senthilkumar.palanivel@gmail.com>
2021-05-23 19:54:04 +0530

    Captcha Deduction ?

--- clear.js
+++ clear.js
@@ -29,6 +29,8 @@ const searchRegExp2 = /path fill=\".+?\"/g;
 const replaceWith2  = 'path fill="#000"';
 const lettersRegExp = /\<path fill.+?\>/g
 
+const lettersMap = new HashMap();
+
 let jwt = require('jwt-simple');
 
 const NodeCache = require("node-cache");
@@ -38,6 +40,15 @@ let onLoad           = true;
 let availabilty      = false;
 let availableSession = 0;
 dotenv.config()
+
+const schemaCaptcha = {
+    properties: {
+        captcha: {
+            description: 'Enter Captcha',
+            required   : true
+        }
+    }
+};
 
 const baseUrl       = 'https://cdn-api.co-vin.in/api/v2';
 const mobile_number = Number(process.env['mobile']);
@@ -66,32 +77,31 @@ jwtCache.on("flush", function () {
 });
 
 
-function SaveCaptchaData(svgData) {
+function SaveCaptchaData(svgData, captchaString) {
     const dir = './letters/'
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
     }
 
     const lettersFound = svgData.match(lettersRegExp);
-    console.log(lettersFound.length);
+
 
     for (let pos = 0; pos < lettersFound.length; pos++) {
-        const id                 = shortid.generate();
-        const letter             = lettersFound[pos];
-        const letterSVG          = `<svg xmlns="http://www.w3.org/2000/svg" width="150" height="50" viewBox="0,0,150,50">${letter}</svg>`;
-        const result             = optimize(letterSVG, {
-            // optional but recommended field
-            path: 'path-to.svg',
-            // all config fields are also available here
-            multipass: true
-        })
-        const optimizedletterSVG = result.data
-
-        fs.writeFile(`${dir}letter${pos}-${id}.svg`, optimizedletterSVG, function (err) {
+        const letter         = lettersFound[pos];
+        const letterSVG      = `<svg xmlns="http://www.w3.org/2000/svg" width="600" height="200" viewBox="0,0,150,50">${letter}</svg>`;
+        const letterPosition = getLetterPosition(letter.slice(0, 48));
+        lettersMap.set(letterPosition, letterSVG);
+    }
+    const letters = lettersMap.keys();
+    letters.sort((a, b) => a - b);
+    for (let pos = 0; pos < letters.length; pos++) {
+        const id             = shortid.generate();
+        const letterPosition = letters[pos];
+        const letterSVG      = lettersMap.get(letterPosition);
+        console.log(letterPosition);
+        fs.writeFile(`${dir}letter${pos}-${id}.svg`, letterSVG, function (err) {
             if (err) throw err;
             sharp(`${dir}letter${pos}-${id}.svg`)
-                .trim()
-                .resize({width: 300})
                 .sharpen()
                 .normalise()
                 .negate()
@@ -102,36 +112,15 @@ function SaveCaptchaData(svgData) {
                     right : 40
                 })
                 .flatten({background: '#FFFFFF'})
-                .jpeg().toFile(`${dir}segment${pos}-${id}.jpeg`)
+                .png()
+                .trim().toFile(`${dir}${captchaString.charAt(pos)}.png`)
                 .then(function (info) {
-                    console.log(info);
-                    // fs.unlinkSync(`${dir}letter${pos}-${id}.svg`);
+                    fs.unlinkSync(`${dir}letter${pos}-${id}.svg`);
                 });
-        });
+        })
     }
-    // looksSame(`${dir}c.png`, `${dir}c_1.png`, {
-    //     tolerance            : 9,
-    //     ignoreAntialiasing   : true,
-    //     antialiasingTolerance: 20
-    // }, function (error, output) {
-    //     // equal will be true, if images looks the same
-    //     console.error(error)
-    //     console.log(chalk.green(JSON.stringify(output, null, 2)));
-    // });
-    //
-    // looksSame.createDiff({
-    //     reference            : `${dir}c.png`,
-    //     current              : `${dir}c_1.png`,
-    //     diff                 : `${dir}c_Diff.png`,
-    //     highlightColor       : '#ff00ff', // color to highlight the differences
-    //     strict               : false, // strict comparsion
-    //     tolerance            : 5,
-    //     antialiasingTolerance: 10,
-    //     ignoreAntialiasing   : true, // ignore antialising by default
-    //     ignoreCaret          : true // ignore caret by default
-    // }, function (error) {
-    //
-    // });
+
+
 }
 
 
@@ -186,14 +175,60 @@ persistence_storage.init({
         headers: Object.assign({}, getOptions.headers, {'If-None-Match': `W/"${crypto.randomBytes(5).toString('hex')}-${crypto.randomBytes(27).toString('hex')}`})
     };
 
-    for (let pos = 0; pos < 2; pos++) {
+    for (let pos = 0; pos < 1; pos++) {
         needle.post(`${baseUrl}/auth/getRecaptcha`, '{}', options, function (err, resp) {
             const responseBody = resp.body;
             const file         = './capcha.svg';
             const svgData      = responseBody.captcha.replace(searchRegExp, replaceWith).replace(searchRegExp2, replaceWith2);
-            SaveCaptchaData(svgData);
+            fs.outputFile(file, svgData, err => {
+                sharp('./capcha.svg')
+                    .resize({height: 50})
+                    .flatten({background: '#e1e1E1'})
+                    .sharpen()
+                    .normalise()
+                    .negate()
+                    .jpeg({
+                        quality: 100
+                    })
+                    .withMetadata({density: 96})
+                    .toFile("./capcha.jpeg")
+                    .then(function (info) {
+                        const result = spawn.sync('catimg', ['-H', '50', './capcha.jpeg'], {stdio: 'inherit'});
+                        prompt.get(schemaCaptcha, function (err, resultCaptcha) {
+                            SaveCaptchaData(svgData, resultCaptcha.captcha);
+                        });
+                    });
+            })
+
         });
     }
+
+    const dir = './letters/'
+
+    // looksSame(`${dir}7.png`, `${dir}7_1.png`, {
+    //     tolerance            : 60,
+    //     ignoreAntialiasing   : true,
+    //     antialiasingTolerance: 80
+    // }, function (error, output) {
+    //     // equal will be true, if images looks the same
+    //     console.error(error)
+    //     console.log(chalk.green(JSON.stringify(output, null, 2)));
+    // });
+    //
+    // looksSame.createDiff({
+    //     reference            : `${dir}7.png`,
+    //     current              : `${dir}7_1.png`,
+    //     diff                 : `${dir}7_Diff.png`,
+    //     highlightColor       : '#ff00ff', // color to highlight the differences
+    //     strict               : false, // strict comparsion
+    //     tolerance            : 60,
+    //     antialiasingTolerance: 80,
+    //     ignoreAntialiasing   : true, // ignore antialising by default
+    //     ignoreCaret          : true // ignore caret by default
+    // }, function (error) {
+    //
+    // });
+    // compare();
 
 });
 
@@ -210,4 +245,44 @@ function with_ordinal(value) {
         return value + "ᴿᴰ";
     }
     return value + "ᵀᴴ";
+}
+
+function getLetterPosition(svgData) {
+    const firstPointRegEx = /d=\"[A-Z](.+?) /g
+    const lettersFound    = firstPointRegEx.exec(svgData);
+    // console.log("svgData " + JSON.stringify(svgData));
+    // console.log("lettersFound " + JSON.stringify(lettersFound));
+
+
+    if (lettersFound !== undefined && lettersFound.length >= 2) {
+        return Number(lettersFound[1]);
+    } else {
+        return 0;
+    }
+}
+
+
+async function compare() {
+    const dir = './letters/'
+
+    const edinburgh_original  = await Jimp.read(`${dir}b.png`);
+    const edinburgh_sharpened = await Jimp.read(`${dir}b_1.png`);
+    const other               = await Jimp.read(`${dir}5.png`);
+
+    console.log("Images compared to edinburgh_original.jpg\n=========================================");
+    console.log(`hash (base 64) ${edinburgh_original.hash()}`);
+    console.log(`hash (binary)  ${edinburgh_original.hash(2)}\n`);
+
+    console.log("edinburgh_sharpened.jpg\n=======================");
+    console.log(`hash (base 64) ${edinburgh_sharpened.hash()}`);
+    console.log(`hash (binary)  ${edinburgh_sharpened.hash(2)}`);
+    console.log(`distance       ${Jimp.distance(edinburgh_original, edinburgh_sharpened)}`);
+    console.log(`diff.percent   ${Jimp.diff(edinburgh_original, edinburgh_sharpened).percent}\n`);
+
+    console.log("other.jpg\n=======================");
+    console.log(`hash (base 64) ${other.hash()}`);
+    console.log(`hash (binary)  ${other.hash(2)}`);
+    console.log(`distance       ${Jimp.distance(edinburgh_original, other)}`);
+    console.log(`diff.percent   ${Jimp.diff(edinburgh_original, other).percent}\n`);
+
 }
